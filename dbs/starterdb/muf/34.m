ab3544fc929aea5542823c8bfeacdf754341167e
Justin Coffman <jcoffman@techsecu.red>
2021-11-01 16:44:38 -0400

    Revert CON* primitive commits

--- dbs/starterdb/muf/34.m
+++ dbs/starterdb/muf/34.m
@@ -71,7 +71,8 @@ $endif
 $iflib $lib/alias
     me @ swap alias-expand  ( Honor aliases issue417 Natasha@HLM 9 January 2003 )
 $else
-    pmatch dup ok? if 1 array_make else pop 0 array_make then
+    .noisy_pmatch
+    dup ok? if 1 array_make else pop "exit" abort then
 $endif
 ;
 : do-watchfor pop me @ prop_wflist array_get_reflist ;
